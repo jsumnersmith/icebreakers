4901131b615126c71a35b18bd8cc3e356673bb4b
Joel Smith <jsumnersmith@gmail.com>
2021-10-07 06:14:25 -0400

    Back to less secure

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl = process.env.SUPABASE_URL
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY
+const supabaseUrl = process.env.GATSBY_SUPABASE_URL
+const supabaseAnonKey = process.env.GATSBY_SUPABASE_ANON_KEY
 
 export default createClient(supabaseUrl, supabaseAnonKey)
